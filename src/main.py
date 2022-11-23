7aaa801ff9c8471e92ee9ac718c4c36d9c154aaf
Matt Revelle <drone@bitrit.es>
2022-11-22 17:07:03 -0700

    Relocated main module.

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ import sys
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QLabel
 
-from window import EmberWindow
+from ember.window import EmberWindow
 
-if __name__ == "__main__":
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
     app = QApplication(sys.argv)
     window = EmberWindow()
     window.show()
     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main(None)
