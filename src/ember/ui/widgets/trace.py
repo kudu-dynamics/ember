355363ca3ebb4362620bc6bb4136fea3f055bfb7
Matt Revelle <drone@bitrit.es>
2022-11-30 16:47:42 -0700

    Latest tweaks to trace view. Rendering with colored margins. Some minor code formatting changes as well.

--- src/ember/ui/widgets/trace.py
+++ src/ember/ui/widgets/trace.py
@@ -1,40 +1,54 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Union
+from random import randint
 
 from PySide6.QtCore import Qt, QRectF
-from PySide6.QtGui import QColor, QPainter, QPen
-from PySide6.QtWidgets import QGraphicsItem, QGraphicsLineItem, QStyleOptionGraphicsItem, QWidget
+from PySide6.QtGui import QBrush, QColor, QPainter, QPen
+from PySide6.QtWidgets import QGraphicsItem, QGraphicsLineItem, QGraphicsRectItem, QGraphicsScene, QStyleOptionGraphicsItem, QWidget
 
 from .graphics import InteractiveGraphicsView
 
-@dataclass
-class TraceSnapshot:
-  instr_addr: int
+class TraceEntry:
+  def __init__(self,
+               start_addr: int,
+               data: str):
+    self.start_addr = start_addr
+    self.data = data
 
+class TraceContext:
+  def __init__(self,
+               name: str,
+               entries: List[Union[TraceEntry, 'TraceContext']]):
+    self.name = name
+    self.entries = entries
 
 class TraceItem(QGraphicsItem):
   def __init__(self,
-               snapshot: TraceSnapshot,
+               trace_entry: TraceEntry,
                width: float,
                height: float,
-               bg_color: QColor,
                parent=None):
     super().__init__(parent=parent)
-    self._snapshot: TraceSnapshot = snapshot
-    self._bg_color: QColor = bg_color
+    self._trace_entry: trace_entry = trace_entry
     self.width = width
     self.height = height
+    self.rect = QGraphicsRectItem(0.0, 0.0, width, height)
+    self.rect.setBrush(QColor(50, 50, 50))
+    self._pen = QPen(QColor(10, 10, 10))
+    self._pen.setWidth(3)
+    self.rect.setPen(self._pen)
 
   def boundingRect(self) -> QRectF:
-    return QRectF(-self.width / 2, -self.height / 2, self.width, self.height)
+    pen_width = self._pen.width()
+    return QRectF(0.0, 0.0, self.width + pen_width, self.height + pen_width)
 
   def paint(self,
             painter: QPainter,
             _option: QStyleOptionGraphicsItem,
             _widget: QWidget):
+    # painter.drawText(boundingRect.center(), hex(self._trace_entry.start_addr))
     boundingRect = self.boundingRect()
-    painter.fillRect(boundingRect, self._bg_color)
-    painter.drawText(boundingRect.center(), hex(self._snapshot.instr_addr))
+    self.rect.paint(painter, _option, _widget)
 
 
 class TraceWidget(InteractiveGraphicsView):
@@ -46,14 +60,61 @@ class TraceWidget(InteractiveGraphicsView):
   the `reload()` method should be called.
   """
 
-  def __init__(self, trace: List[TraceSnapshot], parent=None):
+  def __init__(self, trace: TraceContext, parent=None):
     super().__init__(parent=parent)
 
     # TODO: We could instead provide trace data and then have
     #       this widget construct the TraceItem/QGraphicsItem
     #       that corresponds to it. This gives us a place to
     #       make global decisions.
-    self._trace: List[TraceSnapshot] = trace
+    self._trace: List[TraceContext] = trace
+
+  def load_context(self,
+                   scene: QGraphicsScene,
+                   margin_brush: QBrush,
+                   y_pos: float,
+                   x_left: float,
+                   x_right: float,
+                   x_pos: float,
+                   ctx: TraceContext) -> float:
+      for entry in ctx.entries:
+          if isinstance(entry, TraceContext):
+              # Make recursive call to load nested context
+              x_margin: float = 50.0
+              prev_margin_brush = margin_brush
+              margin_brush = QBrush(QColor(randint(100, 255), randint(100, 255), randint(100, 255)))
+              y_pos = self.load_context(scene, margin_brush, y_pos, x_left, x_right, x_pos + x_margin, entry)
+              margin_brush = prev_margin_brush
+          else:
+              # Position and add the items to the scene
+              width = 500.0
+              height = 100.0
+              item = TraceItem(entry, width, height)
+              item.setPos(x_pos, y_pos)
+              scene.addItem(item)
+              print(f'Add item: {x_pos}, {y_pos}')
+              print(f'Item size: {item.boundingRect()}')
+
+              # Draw margin
+              margin_rect = QGraphicsRectItem(QRectF(0.0, 0.0, x_pos - x_left, height))
+              margin_rect.setPos(x_left, y_pos)
+              margin_rect.setBrush(margin_brush)
+              scene.addItem(margin_rect)
+
+              # Advance y-position
+              y_pos += height
+
+              # Draw the segment divider
+              divider = QGraphicsLineItem(x_pos, y_pos, x_pos + width, y_pos)
+              divider.setPen(QPen(QColor(150, 150, 150)))
+              scene.addItem(divider)
+              print(f'Add divider: {y_pos}')
+
+              # Update the location for the next item
+              DIVIDER_HEIGHT = 6
+              y_pos += DIVIDER_HEIGHT
+
+      return y_pos
 
   def reload(self) -> None:
       self._reset_scene()
@@ -66,6 +127,9 @@ class TraceWidget(InteractiveGraphicsView):
       viewport_rect = self.mapToScene(self.viewport().geometry()).boundingRect()
       print(f'Viewport rect: {viewport_rect}')
 
+      width = 500.0
+      height = 300.0
+
       # Initialize the start positions
       x_left: float = viewport_rect.x()
       x_right: float = x_left + viewport_rect.width()
@@ -75,25 +139,4 @@ class TraceWidget(InteractiveGraphicsView):
 
       print(f'x_left: {x_left}, x_right: {x_right}')
 
-      for snapshot in self._trace:
-          # Position and add the items to the scene
-          item = TraceItem(snapshot, viewport_rect.width(), viewport_rect.height() / 4, QColor('green'))
-          y_pos += item.boundingRect().height() / 2
-          item.setPos(x_mid, y_pos)
-          scene.addItem(item)
-          print(f'Add item: {x_mid}, {y_pos}')
-          print(f'Item size: {item.boundingRect()}')
-
-          # Draw the segment divider
-          y_pos += item.boundingRect().height() / 2
-          divider = QGraphicsLineItem(x_left, y_pos, x_right, y_pos)
-          divider.setPen(QPen(QColor(150, 150, 150)))
-          scene.addItem(divider)
-          print(f'Add divider: {y_pos}')
-
-          # Update the location for the next item
-          DIVIDER_HEIGHT = 6
-          y_pos += DIVIDER_HEIGHT
-
-      viewport_rect = self.mapToScene(self.viewport().geometry()).boundingRect()
-      print(f'After viewport rect: {viewport_rect}')
+      self.load_context(scene, QBrush('black'), y_pos, x_left, x_right, x_left, self._trace)
--- src/ember/window.py
+++ src/ember/window.py
@@ -7,42 +7,59 @@ from PySide6.QtGui import QColor, QFont
 
 from .ui.widgets.log import LogWidget
 from .ui.widgets.graph import FlowGraphWidget
-from .ui.widgets.trace import TraceSnapshot, TraceWidget
+from .ui.widgets.trace import TraceContext, TraceEntry, TraceWidget
 from .log import LogOut
 
 class EmberWindow(QMainWindow):
-  def __init__(self):
-    super().__init__()
+    def __init__(self):
+        super().__init__()
+        self.setup()
 
-    self.setup()
+    def setup(self):
+        self.setGeometry(100, 100, 1024, 768)
+        self.setWindowTitle('Ember')
 
-  def setup(self):
-    self.setGeometry(100, 100, 1024, 768)
-    self.setWindowTitle('Ember')
+        log_w = LogWidget()
+        sys.stdout = LogOut(log_w, sys.stdout)
+        sys.stderr = LogOut(log_w, sys.stderr, QColor(255, 0, 0))
 
-    log_w = LogWidget()
-    sys.stdout = LogOut(log_w, sys.stdout)
-    sys.stderr = LogOut(log_w, sys.stderr, QColor(255, 0, 0))
+        log_dock_w = QDockWidget("Log")
+        log_dock_w.setWidget(log_w)
 
-    log_dock_w = QDockWidget("Log")
-    log_dock_w.setWidget(log_w)
+        graph_w = FlowGraphWidget(None)
+        graph_w.reload()
 
-    graph_w = FlowGraphWidget(None)
-    graph_w.reload()
+        trace = TraceContext('A',
+                             [TraceEntry(0x0, 'a'),
+                              TraceEntry(0x4, 'b'),
+                              TraceContext('B',
+                                           [TraceEntry(0x10, 'a'),
+                                            TraceEntry(0x14, 'b'),
+                                            TraceEntry(0x18, 'c'),
+                                            TraceContext('C',
+                                                         [TraceEntry(0x20, 'a'),
+                                                          TraceContext('D',
+                                                                       [TraceEntry(0x30, 'a'),
+                                                                        TraceEntry(0x34, 'b')]),
+                                                          TraceEntry(0x24, 'b'),
+                                                          TraceEntry(0x28, 'c')]),
+                                            TraceEntry(0x1c, 'd')]),
+                              TraceEntry(0x8, 'c'),
+                              TraceEntry(0xc, 'd'),
+                              TraceContext('BB',
+                                           [TraceEntry(0x10, 'a'),
+                                            TraceEntry(0x14, 'b'),
+                                            TraceEntry(0x18, 'c')])])
+        trace_w = TraceWidget(trace)
+        trace_w.reload()
 
-    items: List[TraceSnapshot] = [TraceSnapshot(addr) for addr in [0x0,
-                                                                   0x4,
-                                                                   0x8]]
-    trace_w = TraceWidget(items)
-    trace_w.reload()
+        tab_w = QTabWidget()
+        tab_w.addTab(graph_w, "Graph View")
+        tab_w.addTab(trace_w, "Trace View")
 
-    tab_w = QTabWidget()
-    tab_w.addTab(graph_w, "Graph View")
-    tab_w.addTab(trace_w, "Trace View")
+        self.setCentralWidget(tab_w)
+        self.addDockWidget(Qt.BottomDockWidgetArea, log_dock_w)
 
-    self.setCentralWidget(tab_w)
-    self.addDockWidget(Qt.BottomDockWidgetArea, log_dock_w)
+        self.addDockWidget(Qt.BottomDockWidgetArea, log_dock_w)
 
-    self.addDockWidget(Qt.BottomDockWidgetArea, log_dock_w)
-
-    self.show()
+        self.show()
